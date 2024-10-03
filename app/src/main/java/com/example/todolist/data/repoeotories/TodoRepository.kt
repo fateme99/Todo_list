152937d09cade69a1482aa819329b0d8a87f7189
fateme99 <fatemeasadi.1033@yahoo.com>
2024-10-03 19:26:22 +0330

    add repository for use and inject taskDao.

---
+++ app/src/main/java/com/example/todolist/data/repoeotories/TodoRepository.kt
@@ -0,0 +1,38 @@
+package com.example.todolist.data.repoeotories
+
+import com.example.todolist.data.TodoDao
+import com.example.todolist.data.models.TodoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class TodoRepository @Inject constructor(private val todoDao: TodoDao) {
+
+    val getAllTasks: Flow<List<TodoTask>> = todoDao.getAllTasks()
+    val sortByLowPriority: Flow<List<TodoTask>> = todoDao.sortByLowPriority()
+    val sortByHighPriority: Flow<List<TodoTask>> = todoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<TodoTask> {
+        return todoDao.getSelectedTask(taskId)
+    }
+
+    suspend fun addTask(task: TodoTask) {
+        todoDao.addTask(task)
+    }
+
+    suspend fun updateTask(task: TodoTask) {
+        todoDao.updateTask(task)
+    }
+
+    suspend fun deleteTask(task: TodoTask) {
+        todoDao.deleteTask(task)
+    }
+
+    suspend fun deleteAllTasks() {
+        todoDao.deleteAllTasks()
+    }
+
+    fun getSearchDatabase(searchQuery: String): Flow<List<TodoTask>> {
+        return todoDao.searchDatabase(searchQuery)
+    }
+
+}
